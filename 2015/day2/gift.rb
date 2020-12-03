3612e507a99f752dad2014b9c4b00daf9205711c
Cakes <curticm1@nationwide.com>
2020-12-03 10:51:47 -0500

    2015 day 2

--- 2015/day2/gift.rb
+++ 2015/day2/gift.rb
@@ -2,10 +2,30 @@ class Gift
   attr_reader :height, :width, :length
 
   def initialize(dimensions)
-    @height, @length, @width = dimensions.split('x')
+    @width, @length, @height = dimensions.split('x').map{|num| num.to_i}.sort
   end
 
   def surface_area
-    2 * height * width + 2 * width * 
+    2 * @height * @width + 2 * @width * @length + 2 * @length * @height
+  end
+
+  def extra
+    @width * @length
+  end
+
+  def perimeter
+    2 * @width + 2 * @length
+  end
+
+  def volume
+    @height * @width * @length
+  end
+
+  def paper_needed
+    surface_area + extra
+  end
+
+  def ribbon_needed
+    perimeter + volume
   end
 end
--- 2015/day2/main.rb
+++ 2015/day2/main.rb
@@ -1,1 +1,15 @@
+require_relative 'gift'
+gifts = File.readlines('input.dat')
 
+paper = 0
+ribbon = 0
+
+gifts.each do |gift_dimensions|
+  gift = Gift.new(gift_dimensions)
+
+  paper += gift.paper_needed
+  ribbon += gift.ribbon_needed
+end
+
+puts paper
+puts ribbon
